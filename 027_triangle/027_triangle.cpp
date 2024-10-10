096e6ea945b13f5dc9cea9dde6941e5d206da96f
BeeEye Dmu <bikang@konyang.ac.kr>
2024-10-10 10:57:30 +0900

    Add files via upload

---
+++ 027_triangle/027_triangle.cpp
@@ -0,0 +1,18 @@
+﻿#include <stdio.h>
+
+int main()
+{
+	int n;
+
+	printf("n 입력 : ");
+	scanf_s("%d", &n);
+
+	// (6)
+	for (int i = n; i >= 1; i--) {
+		for (int j = 1; j <= n - i; j++)
+			printf("_");
+		for (int j = 1; j <= 2 * i - 1; j++)
+			printf("*");
+		printf("\n");
+	}
+}
---
+++ 030_secondMax/030_secondMax.cpp
@@ -0,0 +1,28 @@
+﻿#include <stdio.h>
+
+int main()
+{
+	int a[10];
+	int max, max2;
+
+	printf("10개 숫자 입력 : ");
+	for (int i = 0; i < 10; i++) {
+		scanf_s("%d", &a[i]);
+	}
+
+	// max와 second max를 찾아라
+	max = max2 = a[0];
+
+	for (int i = 1; i < 10; i++) {
+		if (a[i] > max) {
+			max2 = max;
+			max = a[i];
+		}
+		else if (a[i] > max2) {
+			max2 = a[i];
+		}
+		printf("i=%d : max = %d, max2 = %d\n", i, max, max2);
+	}
+
+	printf("max = %d, max2 = %d\n", max, max2);
+}
