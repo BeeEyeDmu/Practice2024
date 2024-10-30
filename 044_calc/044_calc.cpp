3f0c9c3b2cab466ff916a7758e35976d3d4afcd1
BeeEye Dmu <bikang@konyang.ac.kr>
2024-10-30 14:07:15 +0900

    Add files via upload

---
+++ 044_calc/044_calc.cpp
@@ -0,0 +1,36 @@
+﻿#include <stdio.h>
+
+double plus(double, double );
+double minus(double a, double b);
+double multiply(double a, double b);
+double divide(double a, double b);
+
+int main() 
+{
+	double a, b;
+
+	printf("두 숫자를 입력하세요 : ");
+	scanf_s("%lf %lf", &a, &b);
+	
+	printf("더하기 = %lf\n", plus(a, b));
+	printf("빼기 = %lf\n", minus(a, b));
+	printf("곱하기 = %lf\n", multiply(a, b));
+	printf("나누기 = %lf\n", divide(a, b));
+}
+
+double plus(double a, double b) {
+	return a + b;
+}
+
+double minus(double a, double b) {
+	return a - b;
+}
+
+double multiply(double a, double b) {
+	return a * b;
+}
+
+double divide(double a, double b)
+{
+	return a / b;
+}
---
+++ 047_factorial/047_factorial.cpp
@@ -0,0 +1,26 @@
+﻿#include <stdio.h>
+
+long long fact(long long n)
+{
+	if (n == 1)
+		return 1;
+	return fact(n - 1) * n;
+}
+
+int main()
+{
+	printf("%d\n", sizeof(long long));
+
+	// 반복문 사용
+	long long f = 1;	// 곱해나가는 수이므로 1로 초기화한다
+	for (int i = 1; i <= 20; i++) {
+		f = 1;
+		for (int j = 1; j <= i; j++)
+			f *= j;
+		printf("%d! = %lld\n", i, f);
+	}
+
+	// 재귀함수를 사용
+	for(int i=1; i<=20; i++)
+		printf("%d! = %lld\n",i, fact(i));
+}
