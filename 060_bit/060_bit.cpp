0683a62c4bdde9c4cfc095a6c826b94a36edaf67
BeeEye Dmu <bikang@konyang.ac.kr>
2024-11-21 09:17:36 +0900

    Add files via upload

---
+++ 060_bit/060_bit.cpp
@@ -0,0 +1,17 @@
+﻿#include <stdio.h>
+
+int main()
+{
+	int x;
+
+	scanf_s("%d", &x);
+
+	printf("x = %d\n", x);
+	printf("(1 << 2) = %d\n", 1 << 2);
+	printf("%d\n", x & (1 << 2));
+
+	printf("아래 4자리 = %d\n", x & 15);
+
+	for (int i = 1; i <= 4; i++)
+		printf("%d번 시프트 : %d\n",i, x << i);
+}
