9f4620a8a43b3edb705009a07993f7a68167fa54
BeeEye Dmu <bikang@konyang.ac.kr>
2024-12-05 10:48:30 +0900

    Add files via upload

---
+++ 074_MemberClass/074_MemberClass.cpp
@@ -0,0 +1,60 @@
+﻿// 074_MemberClass.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <iostream>
+#include <cstring>
+using namespace std;
+
+class Member {
+private:
+    char* name;
+    int age;
+public:
+    Member(const char* name, int age);
+    ~Member();
+    Member(const Member& rhs);
+    void changeName(const char* name);
+    void Print();
+};
+
+int main()
+{
+    Member m1("duhyun", 20);    // 생성자 실행
+    Member m2("sangwon", 19);   // 생성자 실행
+    Member m3(m1);              // 복사생성자 실행
+
+    m1.Print();
+    m2.Print();
+    m3.Print();
+    m2.changeName("kyubin");
+    m2.Print();
+}
+
+Member::Member(const char* name, int age) {
+    this->age = age;
+    // 깊은 복사
+    this->name = new char[strlen(name) + 1];
+    strcpy_s(this->name, strlen(name) + 1, name); 
+}
+
+Member::~Member() {
+    delete[] name;
+}
+
+Member::Member(const Member& rhs) {
+    age = rhs.age;
+    // 깊은 복사
+    name = new char[strlen(rhs.name) + 1];
+    strcpy_s(name, strlen(rhs.name) + 1, rhs.name);
+}
+
+// 깊은 복사가 필요한데, 그 전에 할당되어 있던 이름을 delete 해주어야 한다
+void Member::changeName(const char* name) {
+    delete[] this->name;
+    this->name = new char[strlen(name) + 1];
+    strcpy_s(this->name, strlen(name) + 1, name);
+}
+
+void Member::Print() {
+    cout << "Name: " << name << endl << "Age: " << age << endl;
+}
---
+++ 084_map/084_map.cpp
@@ -0,0 +1,19 @@
+﻿// 084_map.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <iostream>
+#include <map>
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+    map<string, string> f;
+
+    f["강병익"] = "010-1111-2222";
+    f["조용석"] = "010-3333-4444";
+    f["김병수"] = "010-5555-6666";
+
+    for (auto i : f)
+        cout << i.first << " : " << i.second << endl;
+}
